fd134322385a536ac4709ab1d2ffaec45c0cf8ff
kaifeng <953555581@qq.com>
2021-07-13 14:33:28 +0800

    feat: update

--- test/dtstack.config.js
+++ test/dtstack.config.js
@@ -4,13 +4,13 @@ module.exports = [
     host: "127.0.0.1",
     user: "root",
     sourcePath: "./dist",
-    targetPath: "/data/dtstack/dtlogux/current/test",
+    targetPath: "/test",
   },
   // {
   //   alias: "测试环境",
   //   host: "127.0.0.1",
   //   user: "root",
   //   sourcePath: "./dist",
-  //   targetPath: "/data/dtstack/dtlogux/current/test",
+  //   targetPath: "/test",
   // },
 ];

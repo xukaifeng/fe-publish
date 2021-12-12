aa1db745191e9011eed67b2b56dcf1a144678b33
kaifeng <953555581@qq.com>
2021-12-12 20:38:04 +0800

    update

--- bin/fe-publish.js
+++ bin/fe-publish.js
@@ -2,12 +2,11 @@
 const Service = require("../lib/service");
 try {
   const service = new Service();
-
   const rawArgv = process.argv.slice(2);
   const args = require("minimist")(rawArgv);
   const command = args._[0];
-  console.log("hello1", args);
-  service.run(command, args, rawArgv);
+  // console.log(command, args, rawArgv, process.argv);
+  // service.run(command, args, rawArgv);
 } catch (error) {
   console.log(error);
 }
